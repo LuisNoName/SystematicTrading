72ff97afcffd7d422cf49d33527291362fd3efcd
LuisNoName <luis.safar@me.com>
2022-11-10 17:39:57 +0100

    Updated price data

--- scripts/downloadTickers.py
+++ scripts/downloadTickers.py
@@ -1,18 +1,20 @@
+import datetime
+
 import Packages.DataHandler
 from Packages.DataHandler.yahooData import yahooData
 import pandas as pd
 import os
+from datetime import date
 
 if __name__ == '__main__':
     tickerListPath = os.path.join(os.path.dirname(os.getcwd()), './data/market/tickerList.csv')
 
     handler = yahooData()
     tickers = pd.read_csv(tickerListPath)['Tickers']
-    print(tickers)
     for ticker in tickers:
         try:
-            print(ticker)
-            df = handler.getDailyPrices(ticker, start='2018-01-01', end='2022-01-01')
+            print("Downloading price data for {}".format(ticker))
+            df = handler.getDailyPrices(ticker, start = '2000-01-01', end = date.today())
 
             outname = '{}.csv'.format(ticker)
             outdir = './data/market'
@@ -23,3 +25,4 @@ if __name__ == '__main__':
         except:
             print("Couldn't download data for {}".format(ticker))
 
+    print("Data download finished.")
